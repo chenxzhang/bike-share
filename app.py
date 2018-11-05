95785fda474813c2e337f4b2b3e32fff2c9f295c
Chenxiang Zhang <chenxiang.a.zhang@gmail.com>
2018-11-04 22:55:10 -0500

    images not showing, e branching off to experiment

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-
 import draft
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return str(draft.number_of_regulars("bike_data.csv"))
+    return render_template('index.html')
+    #return str(draft.number_of_regulars("bike_data.csv"))
 
 @app.route('/user/<chen>')
 def show_user_profile(chen):
--- draft.py
+++ draft.py
@@ -4,9 +4,20 @@ import pygeodesy
 import geopy.distance
 import math
 
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 def distance(coordinate1, coordinate2):
     return geopy.distance.vincenty(coordinate1, coordinate2).miles
+
+def dict_to_csv(dic, filename):
+    with open(filename,'w') as f:
+        w = csv.writer(f)
+        w.writerows(dic.items())
 
 def readCSVSkipOneLine2(filename):
     result=[]
@@ -34,9 +45,22 @@ def popular_starting(filename):
                 mydict[row[row_index]] += 1
             else:
                 mydict[row[row_index]] = 1
-    station = max(mydict.items(), key=operator.itemgetter(1))[0]
-        #mydict = {rows[0]:rows[1] for rows in reader}
-    return station
+    sorted_dict = sorted(mydict, key=mydict.get, reverse=True)
+    station_ids = []
+    number_of_trips = []
+    y_pos = np.arange(10)
+    for i in range(10):
+        station_id =sorted_dict[i]
+        station_ids.append(station_id)
+        number_of_trips.append(mydict[station_id])
+
+    plt.bar(y_pos, number_of_trips, align='center', alpha=0.35)
+    plt.xticks(y_pos, tuple(station_ids))
+    plt.ylabel('Number of Trips')
+    plt.xlabel('Station ID')
+    plt.title('Top Ten Most Popular Starting Station')
+    plt.savefig("static/graphs/popular_starting.png", bbox_inches="tight", format="png",)
+
 
 #Most popular ending station
 def popular_ending(filename):
@@ -49,15 +73,23 @@ def popular_ending(filename):
                 mydict[row[row_index]] += 1
             else:
                 mydict[row[row_index]] = 1
-    station = max(mydict.items(), key=operator.itemgetter(1))[0]
-        #mydict = {rows[0]:rows[1] for rows in reader}
 
-    return station
+    sorted_dict = sorted(mydict, key=mydict.get, reverse=True)
+    station_ids = []
+    number_of_trips = []
+    y_pos = np.arange(10)
+    for i in range(10):
+        station_id =sorted_dict[i]
+        station_ids.append(station_id)
+        number_of_trips.append(mydict[station_id])
 
-def write_distance_frequence(dic):
-    w = csv.writer(open("output.csv", "w"))
-    for key, val in dic.items():
-        w.writerow([key, val])
+    plt.bar(y_pos, number_of_trips, align='center', alpha=0.35)
+    plt.xticks(y_pos, tuple(station_ids))
+    plt.xlabel('Station ID')
+    plt.ylabel('Number of Trips')
+    plt.title('Top Ten Most Popular Ending Station')
+    plt.savefig("static/graphs/popular_ending.png", bbox_inches="tight", format="png",)
+
 
 
 def distance_frequency(dic, distance):
@@ -83,10 +115,8 @@ def average_distance(filename):
             number_of_entries += 1
             start_station_id = row[4]
             end_station_id = row[7]
-            lat1 = row[5]
-            lon1 = row[6]
-            lat2 = row[8]
-            lon2 = row[9]
+            lat1,lon1 = row[5], row[6]
+            lat2,lon2 = row[8], row[9]
             #Not enough information given
             if (start_station_id == "" or end_station_id == "" or lat1 == ""
                 or lat1 == "0" or lon1 == "" or lon1 == "0" or lat2 == "" 
@@ -117,15 +147,55 @@ def average_distance(filename):
                 trip_distance = distance(start_station,end_station)
                 distance_frequency(distance_dictionary, trip_distance)
                 result+=trip_distance
-    write_distance_frequence(distance_dictionary)
+    sorted_dict = sorted(distance_dictionary.items(), key=operator.itemgetter(0))
+
+    average = result/number_of_entries
+    average_distance = []
+    frequency = []
+    for i in range(len(sorted_dict)):
+        xdistance = sorted_dict[i][0]
+        average_distance.append(xdistance)
+        frequency.append(sorted_dict[i][1])
+    print(average_distance)
+    print(frequency)
+    # Create data
+    N = len(distance_dictionary)
+    x = average_distance
+    y = frequency
+    y_pos = np.arange(N)
+    colors = (0,0,0)
+    area = np.pi*3
+     
+    # Plot
+    
+    plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+    plt.plot(x, y, linestyle='-')
+    plt.title('Distance Distribution')
+    plt.xlabel('Distance(miles)')
+    plt.ylabel('Frequency')
+    plt.savefig("static/graphs/distance.png", bbox_inches="tight", format="png",)
     return result/number_of_entries
 
 def number_of_regulars(filename):
     pass_index = 13
     number = 0
+    total = 0
     with open(filename, mode='r') as infile:
         reader = csv.reader(infile)
         for row in reader:
+            total+=1
             if row[pass_index]=="Flex Pass" or row[pass_index]=="Monthly Pass":
                 number+=1
+    # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+    labels = 'Regular', 'Non-Regular'
+    sizes = [number, total-number]
+    
+    
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    ax1.set_title("Regular vs. Non-Regular Riders")
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.savefig("static/graphs/regular_rider.png", bbox_inches="tight", format="png",)
     return number
+
+
